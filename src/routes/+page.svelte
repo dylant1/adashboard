27cde7666bf9b8447e6d465bdca873a41b91dfe7
Dylan Toth <dylant1@yahoo.com>
2023-01-24 00:11:29 -0500

    Setup sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<script lang='ts'>
+  import type { PageData } from './$types';
+  export let data: PageData;
+  let name = 'world';
+
+</script>
+
+<p>Visit {name} to read the documentation</p>
+  <h1> {data.hello} </h1>
+
+
+
+
